b9c7bf8b3fd0aeea0c43d104ce510ea69ad8452f
Max Derner <max.derner@gmail.com>
2024-11-22 12:41:35 +0000

    fixed gradient technique

--- POCs/techniques.py
+++ POCs/techniques.py
@@ -1,36 +1,84 @@
+from math import ceil
+from bansi_codes import Style, compile_ansi_code
 
 
-def apply_gradient(text: str, colour_codes: list):
+def apply_vertical_gradient(
+        text: str,
+        ansi_codes: list,
+        *,
+        step: int = 1,
+        indent: int = 1,
+        ) -> str:
     lines_of_text = text.split('\n')
     text_length = max(
         [len(line) for line in lines_of_text]
     )
-    colour_codes = colour_codes[:text_length]
-    colour_length = text_length // len(colour_codes)
+    colour_array = _build_colour_array(text_length, ansi_codes)
     output = ''
-    for line in lines_of_text:
-        colour_pos = 0
-        for char_pos, char in enumerate(line):
-            if char_pos % colour_length == 0:
-                output += colour_codes[colour_pos]
-                colour_pos += 1
-                colour_pos = min(len(colour_codes) - 1, colour_pos)
-            output += char
-        output += '\n'
+    current_code = None
+    RESET = compile_ansi_code()
+    for line_no, line in enumerate(lines_of_text):
+        if line_no != 0 and line_no % step == 0:
+            colour_array = colour_array[indent:] + colour_array[:indent]
+        for idx, char in enumerate(line):
+            idx = idx % len(colour_array)
+            if current_code != (new_code := colour_array[idx]):
+                current_code = new_code
+                output += current_code + char
+            else:
+                output += char
+        output += RESET + '\n'
+        current_code = None
     return output
 
 
+def _build_colour_array(
+        array_length: int,
+        ansi_codes: list[str],
+        ) -> list[str]:
+    """expands a list of ANSI codes to a new length such that any code
+    appearing before another code in the original list will not appear 
+    after that code in the new list.
+
+    Example:
+
+    [1, 2, 3] with new length 6 gives [1, 1, 2, 2, 3, 3]"""
+    colour_length = ceil(array_length / len(ansi_codes))
+    colour_array = [
+        ansi_code
+        for ansi_code in ansi_codes
+        for _ in range(colour_length)
+    ]
+    return colour_array
+
+
 if __name__ == "__main__":
-    from bansi_codes import Colour, compile_ansi_code
+    from bansi_codes import Colour
     from pathlib import Path
 
-    red = compile_ansi_code(foreground=Colour.RED)
-    orange = compile_ansi_code(foreground=Colour.RED.bright)
-    yellow = compile_ansi_code(foreground=Colour.YELLOW.bright)
-    green = compile_ansi_code(foreground=Colour.GREEN)
-    blue = compile_ansi_code(foreground=Colour.BLUE)
-    indigo = compile_ansi_code(foreground=Colour.BLUE.bright)
-    violet = compile_ansi_code(foreground=Colour.MAGENTA)
+    red = compile_ansi_code(Colour.RED, Style.BOLD)
+    orange = compile_ansi_code(Colour.RED.bright, Style.BOLD)
+    yellow = compile_ansi_code(Colour.YELLOW.bright, Style.BOLD)
+    green = compile_ansi_code(Colour.GREEN, Style.BOLD)
+    blue = compile_ansi_code(Colour.BLUE, Style.BOLD)
+    indigo = compile_ansi_code(Colour.BLUE.bright, Style.BOLD)
+    violet = compile_ansi_code(Colour.MAGENTA, Style.BOLD)
+
+    # red = compile_ansi_code(Colour.RED.background)
+    # orange = compile_ansi_code(Colour.RED.bright_background)
+    # yellow = compile_ansi_code(Colour.YELLOW.bright_background)
+    # green = compile_ansi_code(Colour.GREEN.background)
+    # blue = compile_ansi_code(Colour.BLUE.background)
+    # indigo = compile_ansi_code(Colour.BLUE.bright_background)
+    # violet = compile_ansi_code(Colour.MAGENTA.background)
+
+    # red = compile_ansi_code(Colour.RED.background, Style.BLINK)
+    # orange = compile_ansi_code(Colour.RED.bright_background, Style.BLINK)
+    # yellow = compile_ansi_code(Colour.YELLOW.bright_background, Style.BLINK)
+    # green = compile_ansi_code(Colour.GREEN.background, Style.BLINK)
+    # blue = compile_ansi_code(Colour.BLUE.background, Style.BLINK)
+    # indigo = compile_ansi_code(Colour.BLUE.bright_background, Style.BLINK)
+    # violet = compile_ansi_code(Colour.MAGENTA.background, Style.BLINK)
 
     codes = [
         red,
@@ -42,8 +90,13 @@ if __name__ == "__main__":
         violet,
     ]
 
-    sample_path = Path(__file__).parent.joinpath('sample.txt')
+    sample_path = Path(__file__).parent.joinpath('promo_sample.txt')
     with open(sample_path, mode='r') as f:
         text = f.read()
 
-    print(apply_gradient(text, codes))
+    output = apply_vertical_gradient(text, codes, step=1, indent=11)
+    from pathlib import Path
+    output_path = Path(__file__).parent.parent.joinpath('zzz').joinpath('output.txt')
+    with open(output_path, mode='w') as f:
+        f.write(output)
+    print(output)
