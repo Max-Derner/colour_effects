1e73a27590612afee96afacb77a91f76f43be6c6
Max Derner <max.derner@gmail.com>
2024-11-25 11:24:03 +0000

    introduced typing and a bit more abstraction so that it's easier to combine colour effects

--- POCs/techniques.py
+++ POCs/techniques.py
@@ -2,11 +2,33 @@ from math import ceil
 from bansi_codes import Style, compile_ansi_code
 
 
-# This whole thing needs a rejig
-#
-# These apply functions should simply add ansi vals into a 3D array of
-# array[lines][columns][vals]
-# Apply ansi codes should then compile the vals into codes
+type ansi_sequence = list[list[str]]
+type ansi_field = list[ansi_sequence]
+
+
+def _produce_ansi_field(
+        template: str | ansi_field,
+        ) -> tuple[int, ansi_field]:
+    """accepts either the text for which you wish to create an ANSI
+    field, or a previous ANSI field you want copying"""
+    if isinstance(template, str):
+        split_text = template.split('\n')
+        no_lines = len(split_text)
+        no_cols = max(
+            [len(line) for line in split_text]
+        )
+        return [
+            [[] for _ in range(no_cols)]
+            for _ in range(no_lines)
+        ]
+    elif isinstance(template, ansi_field):
+        return [
+            col.copy()
+            for line in template
+            for col in line
+        ]
+    else:
+        raise TypeError("template not 'str' or 'ansi_field'")
 
 
 def apply_shimmer(text: str, spread: int) -> str:
@@ -25,76 +47,76 @@ def apply_shimmer(text: str, spread: int) -> str:
     return output
 
 
-def apply_vertical_gradient(
-        text: str,
-        ansi_codes: list,
+def create_vertical_gradient(
+        template: str | ansi_field,
+        ansi_codes: ansi_sequence,
         *,
         step: int = 1,
-        indent: int = 1,
-        ) -> str:
-    lines_of_text = text.split('\n')
-    text_length = max(
-        [len(line) for line in lines_of_text]
-    )
-    ansi_codes = _expand_ansi_codes(text_length, ansi_codes)
+        indent: int = 0,
+        ) -> ansi_field:
+    field = _produce_ansi_field(template)
+    field_height = len(field)
+    field_width = len(field[0])
+    ansi_codes = _expand_ansi_codes(field_width, ansi_codes)
 
-    code_array = []
-    for line_no in range(len(lines_of_text)):
+    for line_no in range(field_height):
         if line_no != 0 and line_no % step == 0:
             ansi_codes = ansi_codes[indent:] + ansi_codes[:indent]
-        code_array.append(ansi_codes)
+        for col_no in range(field_width):
+            field[line_no][col_no].extend(ansi_codes[col_no])
 
-    return apply_ansi_codes(lines_of_text, code_array)
+    return field
 
 
-def apply_horizontal_gradient(
-        text: str,
-        ansi_codes: list,
+def create_horizontal_gradient(
+        template: str | ansi_field,
+        ansi_codes: ansi_sequence,
         *,
         step: int = 1,
-        indent: int = 1,
-        ) -> str:
-    lines_of_text = text.split('\n')
-    text_depth = len(lines_of_text)
-    text_length = max(
-        [len(line) for line in lines_of_text]
-    )
-    ansi_codes = _expand_ansi_codes(text_depth, ansi_codes)
+        indent: int = 0,
+        ) -> ansi_field:
+    field = _produce_ansi_field(template)
+    field_height = len(field)
+    field_width = len(field[0])
+    ansi_codes = _expand_ansi_codes(field_height, ansi_codes)
 
     array_bias = []
-    for col_no in range(text_length):
+    for col_no in range(field_width):
         step_no = col_no // step
         array_bias.append(step_no * indent)
 
-    code_array = []
-    for line_no in range(text_depth):
-        code_line = []
-        for col_no in range(text_length):
+    for line_no in range(field_height):
+        for col_no in range(field_width):
             code_idx = (line_no + array_bias[col_no]) % len(ansi_codes)
-            code_line.append(ansi_codes[code_idx])
-        code_array.append(code_line)
+            field[line_no][col_no].extend(ansi_codes[code_idx])
 
-    return apply_ansi_codes(lines_of_text, code_array)
+    return field
 
 
-def apply_ansi_codes(
-        lines_of_text: list[str],
-        ansi_array: list[list[str]]
+def apply_ansi_field(
+        text: str,
+        field: ansi_field,
         ) -> str:
+    lines_of_text = text.split('\n')
     output = ''
-    current_code = None
+    current_ansi_vals = None
     RESET = compile_ansi_code()
     for line_no, line in enumerate(lines_of_text):
-        codes = ansi_array[line_no]
+        line_no = line_no % len(field)
+        sequence = field[line_no]
         for idx, char in enumerate(line):
-            idx = idx % len(codes)
-            if current_code != (new_code := codes[idx]):
-                current_code = new_code
-                output += current_code + char
+            idx = idx % len(sequence)
+            if current_ansi_vals != (new_vals := sequence[idx]):
+                current_ansi_vals = new_vals
+                output += (
+                    RESET
+                    + compile_ansi_code(*current_ansi_vals)
+                    + char
+                )
             else:
                 output += char
         output += RESET + '\n'
-        current_code = None
+        current_ansi_vals = None
     return output
 
 
@@ -122,13 +144,13 @@ if __name__ == "__main__":
     from eight_bit_codes import SimpleColour, RGB, Grey
     from pathlib import Path
 
-    # red = compile_ansi_code(Colour.RED, Style.BOLD)
-    # orange = compile_ansi_code(Colour.RED.bright, Style.BOLD)
-    # yellow = compile_ansi_code(Colour.YELLOW.bright, Style.BOLD)
-    # green = compile_ansi_code(Colour.GREEN, Style.BOLD)
-    # blue = compile_ansi_code(Colour.BLUE, Style.BOLD)
-    # indigo = compile_ansi_code(Colour.BLUE.bright, Style.BOLD)
-    # violet = compile_ansi_code(Colour.MAGENTA, Style.BOLD)
+    red = [Colour.RED, Style.BOLD]
+    orange = [Colour.RED.bright, Style.BOLD]
+    yellow = [Colour.YELLOW.bright, Style.BOLD]
+    green = [Colour.GREEN, Style.BOLD]
+    blue = [Colour.BLUE, Style.BOLD]
+    indigo = [Colour.BLUE.bright, Style.BOLD]
+    violet = [Colour.MAGENTA, Style.BOLD]
 
     # red = compile_ansi_code(SimpleColour.RED, SimpleColour.WHITE.background, Style.BOLD)
     # orange = compile_ansi_code(SimpleColour.RED.bright, Style.BOLD)
@@ -138,13 +160,13 @@ if __name__ == "__main__":
     # indigo = compile_ansi_code(SimpleColour.BLUE.bright, Style.BOLD)
     # violet = compile_ansi_code(SimpleColour.MAGENTA, Style.BOLD)
 
-    red = compile_ansi_code(RGB(6, 0, 0).foreground, Style.BOLD)
-    orange = compile_ansi_code(RGB(6, 4, 4).foreground, Style.BOLD)
-    yellow = compile_ansi_code(RGB(0, 6, 6).foreground, Style.BOLD)
-    green = compile_ansi_code(RGB(0, 6, 0).foreground, Style.BOLD)
-    blue = compile_ansi_code(RGB(0, 0, 6).foreground, Style.BOLD)
-    indigo = compile_ansi_code(RGB(4, 4, 6).foreground, Style.BOLD)
-    violet = compile_ansi_code(RGB(6, 6, 0).foreground, Style.BOLD)
+    # red = [RGB(6, 0, 0).foreground, Style.BOLD]
+    # orange = [RGB(6, 4, 4).foreground, Style.BOLD]
+    # yellow = [RGB(0, 6, 6).foreground, Style.BOLD]
+    # green = [RGB(0, 6, 0).foreground, Style.BOLD]
+    # blue = [RGB(0, 0, 6).foreground, Style.BOLD]
+    # indigo = [RGB(4, 4, 6).foreground, Style.BOLD]
+    # violet = [RGB(6, 6, 0).foreground, Style.BOLD]
 
     # red = compile_ansi_code(Colour.RED.background)
     # orange = compile_ansi_code(Colour.RED.bright_background)
@@ -171,31 +193,41 @@ if __name__ == "__main__":
         indigo,
         violet,
     ]
-    grey_codes = [
-        compile_ansi_code(Grey.A.foreground, Style.BOLD),
-        compile_ansi_code(Grey.B.foreground, Style.BOLD),
-        compile_ansi_code(Grey.C.foreground, Style.BOLD),
-        compile_ansi_code(Grey.D.foreground, Style.BOLD),
-        compile_ansi_code(Grey.E.foreground, Style.BOLD),
-        compile_ansi_code(Grey.F.foreground, Style.BOLD),
-        compile_ansi_code(Grey.G.foreground, Style.BOLD),
-        compile_ansi_code(Grey.H.foreground, Style.BOLD),
-        compile_ansi_code(Grey.I.foreground, Style.BOLD),
-        compile_ansi_code(Grey.J.foreground, Style.BOLD),
-        compile_ansi_code(Grey.K.foreground, Style.BOLD),
-        compile_ansi_code(Grey.L.foreground, Style.BOLD),
-        compile_ansi_code(Grey.M.foreground, Style.BOLD),
-        compile_ansi_code(Grey.N.foreground, Style.BOLD),
-        compile_ansi_code(Grey.O.foreground, Style.BOLD),
-        compile_ansi_code(Grey.P.foreground, Style.BOLD),
-        compile_ansi_code(Grey.Q.foreground, Style.BOLD),
-        compile_ansi_code(Grey.R.foreground, Style.BOLD),
-        compile_ansi_code(Grey.S.foreground, Style.BOLD),
-        compile_ansi_code(Grey.T.foreground, Style.BOLD),
-        compile_ansi_code(Grey.U.foreground, Style.BOLD),
-        compile_ansi_code(Grey.V.foreground, Style.BOLD),
-        compile_ansi_code(Grey.W.foreground, Style.BOLD),
-        compile_ansi_code(Grey.X.foreground, Style.BOLD),
+    grey_sequence: ansi_sequence = [
+        [Grey.A, Style.BOLD],
+        [Grey.B, Style.BOLD],
+        [Grey.C, Style.BOLD],
+        [Grey.D, Style.BOLD],
+        [Grey.E, Style.BOLD],
+        [Grey.F, Style.BOLD],
+        [Grey.G, Style.BOLD],
+        [Grey.H, Style.BOLD],
+        [Grey.I, Style.BOLD],
+        [Grey.J, Style.BOLD],
+        [Grey.K, Style.BOLD],
+        [Grey.L, Style.BOLD],
+        [Grey.M, Style.BOLD],
+        [Grey.N, Style.BOLD],
+        [Grey.O, Style.BOLD],
+        [Grey.P, Style.BOLD],
+        [Grey.Q, Style.BOLD],
+        [Grey.R, Style.BOLD],
+        [Grey.S, Style.BOLD],
+        [Grey.T, Style.BOLD],
+        [Grey.U, Style.BOLD],
+        [Grey.V, Style.BOLD],
+        [Grey.W, Style.BOLD],
+        [Grey.X, Style.BOLD],
+    ]
+
+    grey_sequence = (
+        grey_sequence
+        + grey_sequence[-2: 0:-1]
+    )
+
+    small_field = [
+        [[Colour.RED.bright, Style.BOLD], [Colour.GREEN, Style.BLINK]],
+        [[Colour.GREEN, Style.BLINK], [Colour.RED.bright, Style.BOLD]],
     ]
 
     sample_path = Path(__file__).parent.joinpath('sample_b.txt')
@@ -206,7 +238,19 @@ if __name__ == "__main__":
     indent = -1
 
     # output = apply_shimmer(text, 3)
-    output = apply_vertical_gradient(text, grey_codes, step=1, indent=2)
+    # gradient_field = create_vertical_gradient(
+    #     text,
+    #     grey_sequence,
+    #     step=1,
+    #     indent=0
+    # )
+    gradient_field = create_horizontal_gradient(
+        text,
+        colour_codes,
+        step=1,
+        indent=0
+    )
+    output = apply_ansi_field(text, small_field)
 
     from pathlib import Path
     output_path = Path(
