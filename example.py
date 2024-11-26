f523e1b5ea72951882654e6595ebd0ff61091ff5
Max Derner <max.derner@gmail.com>
2024-11-26 12:56:57 +0000

    changed example to use 4 bit codes

--- example.py
+++ example.py
@@ -1,9 +1,20 @@
+from colour_fx.four_bit import Colour
+from colour_fx import compile_ansi_code
+
+RESET = compile_ansi_code()
+red = compile_ansi_code(Colour.RED)
+orange = compile_ansi_code(Colour.RED.bright)
+yellow = compile_ansi_code(Colour.YELLOW.bright)
+green = compile_ansi_code(Colour.GREEN)
+blue = compile_ansi_code(Colour.BLUE)
+indigo = compile_ansi_code(Colour.BLUE.bright)
+violet = compile_ansi_code(Colour.MAGENTA.bright)
 
 
-print("\33[38;5;196m         ::::::::      :::       :::   :::   :::::::::  :::        :::::::::: ")  # r
-print("\33[38;5;202m       :+:    :+:   :+: :+:    :+:+: :+:+:  :+:    :+: :+:        :+:         ")  # o
-print("\33[38;5;226m      +:+         +:+   +:+  +:+ +:+:+ +:+ +:+    +:+ +:+        +:+          ")  # y
-print("\33[38;5;46m     +#++:++#++ +#++:++#++: +#+  +:+  +#+ +#++:++#+  +#+        +#++:++#      ")  # g
-print("\33[38;5;21m           +#+ +#+     +#+ +#+       +#+ +#+        +#+        +#+            ")  # b
-print("\33[38;5;129m   #+#    #+# #+#     #+# #+#       #+# #+#        #+#        #+#             ")  # i
-print("\33[38;5;201m   ########  ###     ### ###       ### ###        ########## ##########   ")  # v
+print(F"{red}         ::::::::      :::       :::   :::   :::::::::  :::        :::::::::: {RESET}")  # r
+print(F"{orange}       :+:    :+:   :+: :+:    :+:+: :+:+:  :+:    :+: :+:        :+:         {RESET}")  # o
+print(F"{yellow}      +:+         +:+   +:+  +:+ +:+:+ +:+ +:+    +:+ +:+        +:+          {RESET}")  # y
+print(F"{green}     +#++:++#++ +#++:++#++: +#+  +:+  +#+ +#++:++#+  +#+        +#++:++#      {RESET}")  # g
+print(F"{blue}           +#+ +#+     +#+ +#+       +#+ +#+        +#+        +#+            {RESET}")  # b
+print(F"{indigo}   #+#    #+# #+#     #+# #+#       #+# #+#        #+#        #+#             {RESET}")  # i
+print(F"{violet}   ########  ###     ### ###       ### ###        ########## ##########       {RESET}")  # v
