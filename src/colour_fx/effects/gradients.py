f84a725d44ee8ae667b83d1685b165c70d45eeb4
Max Derner <max.derner@gmail.com>
2024-11-28 12:14:45 +0000

    Added horizontal gradient

--- src/colour_fx/effects/gradients.py
+++ src/colour_fx/effects/gradients.py
@@ -18,12 +18,36 @@ def create_vertical_gradient(
     field = produce_ansi_field(template)
     field_height = len(field)
     field_width = len(field[0])
-    ansi_codes = stretch_ansi_val_collection(field_width, ansi_vals)
+    ansi_vals = stretch_ansi_val_collection(field_width, ansi_vals)
 
     for line_no in range(field_height):
         if line_no != 0 and line_no % step == 0:
-            ansi_codes = ansi_codes[indent:] + ansi_codes[:indent]
+            ansi_vals = ansi_vals[indent:] + ansi_vals[:indent]
         for col_no in range(field_width):
-            field[line_no][col_no].extend(ansi_codes[col_no])
+            field[line_no][col_no].extend(ansi_vals[col_no])
 
+    return field
+
+
+def create_horizontal_gradient(
+        template: Union[str, ansi_field],
+        ansi_vals: ansi_val_collection,
+        *,
+        step: int = 1,
+        indent: int = 0,
+) -> ansi_field:
+    field = produce_ansi_field(template)
+    field_height = len(field)
+    field_width = len(field[0])
+    ansi_vals = stretch_ansi_val_collection(field_height, ansi_vals)
+
+    array_bias = []
+    for col_no in range(field_width):
+        step_no = col_no // step
+        array_bias.append(step_no * indent)
+
+    for line_no in range(field_height):
+        for col_no in range(field_width):
+            code_idx = (line_no + array_bias[col_no]) % len(ansi_vals)
+            field[line_no][col_no].extend(ansi_vals[code_idx])
     return field
